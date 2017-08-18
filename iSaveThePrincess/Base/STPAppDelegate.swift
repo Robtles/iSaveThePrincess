eadf10577c8f4489e7f315bdf23468b38bd2de74
StratRob <robbeyroad@gmail.com>
2017-08-18 23:44:14 +0200

    Reset the project base

---
+++ iSaveThePrincess/Base/STPAppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  iSaveThePrincess
+//
+//  Created by Robin on 18/08/2017.
+//  Copyright © 2017 Rob. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
